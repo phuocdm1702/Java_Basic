914db48c4f1b2ea22746bb2da6418b48bd3657ae
phuocdm1702 <githupstudy@gmail.com>
2025-04-15 01:38:46 +0700

    Commit Math

--- src/tuhoc_java_buoi_1/TuHoc_Java_Buoi_1.java
+++ src/tuhoc_java_buoi_1/TuHoc_Java_Buoi_1.java
@@ -16,6 +16,12 @@ public class TuHoc_Java_Buoi_1 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World");
+        int 
+                a = 5,
+                b = 10,
+                c = a + b
+                ;
+        System.out.println("c=" + c);
     }
 
 }
